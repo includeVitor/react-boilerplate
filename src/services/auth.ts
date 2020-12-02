201a8698ddc253797e3a04f01cebb3787a2337ac
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-12-02 15:31:54 -0300

    trying to make this work

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,25 +1,15 @@
+import api from './api'
+
 export const authService = {
     login
 }
 
-function login(email: string, password: string): Promise<Response> {
-    const requestOptions = {
-        method: 'POST',
-        headers: [['Content-Type', 'application/json'], ['Accept', 'application/json']],
-        body: JSON.stringify({
-            user: { email: email, password: password }
-        })
-    };
+async function login(email: string, password: string){
 
-    return fetch(`/users/sign_in.json`, requestOptions)
-        .catch(() => {
-            return Promise.reject('Backend not reachable');
-        })
-        .then(function (response: Response): any {
-            if (!response.ok) {
-                return Promise.reject(response.statusText);
-            }
-
-            return (response.headers.get('Authorization') || '').split(' ')[1];
-        })
+    try{
+        let result = await api.post(`/login`,{email :email, password : password})
+        console.log(result)
+    }catch(error){
+        console.log(error)
+    }
 }
