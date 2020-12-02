17571bfbcf556124e4548c271bbca70139ea2c18
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-12-02 13:36:57 -0300

    handleclick

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,16 @@
 import React from "react"
 
 const LoginPage: React.FC = () => {
+
+    const handleClick = () => {
+        alert("Clicked")
+    }
+
     return (  
 
         <div>
             This is the login LoginPage
-            <button>Entrar</button>
+            <button onClick={handleClick}>Entrar</button>
         </div>
     );
 }
